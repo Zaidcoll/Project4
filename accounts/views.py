2c0c1e1b9a6684e749e1dbe9c0e3e56dce2ece7f
Ubuntu <ubuntu@ip-172-31-31-145.ec2.internal>
2019-11-19 04:37:19 +0000

    only show links when user is log in or not

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render,redirect, reverse, HttpResponse
 from django.contrib import auth, messages
+from .forms import UserLoginForm
 
 
 # Create your views here.
@@ -14,4 +15,20 @@ def logout(request):
     return redirect(index)
     
 def login(request):
-    return render(request,'accounts/login.html')
+    if request.method == 'POST':
+        login_form = UserLoginForm(request.POST)
+        if login_form.is_valid():
+            #check if username and password matches
+            user = auth.authenticate(username = request.POST['username'],password = request.POST['password'])
+            if user:
+                auth.login(user = user, request = request)
+                return redirect(index)
+                                     
+        else:
+            pass
+            
+    else:
+        form = UserLoginForm()
+        return render(request,'accounts/login.html',{
+            'form':form
+        })

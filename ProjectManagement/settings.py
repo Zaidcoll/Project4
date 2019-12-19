0f6235d574f50cddc4af6b24730d332e1705d619
Ubuntu <ubuntu@ip-172-31-31-145.ec2.internal>
2019-12-19 18:56:53 +0000

    Updated allowed host for heroku

--- ProjectManagement/settings.py
+++ ProjectManagement/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY=os.environ['DJANGO_SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*','https://tshirt-project4.herokuapp.com/']
+ALLOWED_HOSTS = ['https://tshirt-project4.herokuapp.com/']
 
 
 # Application definition
